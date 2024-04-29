5fcaa9dd4fc89e98fec44e08ec4c8fa47b5504b6
Franklin Ramos <franklinramos@outlook.com>
2024-04-28 21:32:17 -0300

    refactor: update layout and add Accordion in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,12 @@ import { Nav } from '@/components/nav'
 import { IP } from '@/components/ip'
 import { siteConfig } from '@/config/site'
 import { Skeleton } from '@/components/ui/skeleton'
-
-export const revalidate = 3600 // 1 hour
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
 
 export default function Home() {
   return (
@@ -13,11 +17,11 @@ export default function Home() {
       <div className="top-0 mx-auto flex w-full max-w-3xl sm:sticky">
         <Nav />
       </div>
-      <div className="container relative mx-auto flex max-w-3xl flex-1 flex-col gap-8 rounded-lg border border-border p-4 backdrop-blur-[2px]">
-        <div className="mx-auto mt-12 max-w-2xl gap-4 text-center">
+      <div className="container relative mx-auto flex max-w-3xl flex-1 flex-col justify-between gap-8 rounded-lg border border-border p-4 backdrop-blur-[2px]">
+        <div className="mx-auto mt-12 min-h-full max-w-2xl flex-1 gap-4 text-center">
           <h1 className="text-3xl">{siteConfig.name}</h1>
-          <section className="flex min-h-60 flex-col items-center justify-center p-4">
-            <div className="flex flex-col items-center justify-center gap-3 p-2">
+          <section className="flex flex-col items-center justify-center p-4">
+            <div className="flex min-h-60 flex-col items-center justify-center gap-3 p-2">
               <label>Your public IP address:</label>
               <Suspense fallback={<Skeleton className="h-14 w-60" />}>
                 <IP />
@@ -25,25 +29,41 @@ export default function Home() {
             </div>
           </section>
         </div>
-        <Skeleton className="h-40 w-full" />
-        <article className="flex min-h-full flex-1 flex-col">
-          <div className="flex flex-col gap-3 border-t border-border pt-4">
-            <h3 className="flex items-center gap-1 text-base">
-              <Icons.info /> More about IP addresses
-            </h3>
-          </div>
+
+        <Skeleton className="h-60 w-full" />
+
+        <article className="flex flex-col">
+          <Accordion type="single" collapsible defaultValue="item-1">
+            <AccordionItem value="item-1">
+              <AccordionTrigger>
+                <span className="flex items-center gap-1 text-base">
+                  <Icons.info /> More about IP addresses
+                </span>
+              </AccordionTrigger>
+              <AccordionContent>
+                <p className="text-sm">
+                  An Internet Protocol (IP) address is a numerical label
+                  assigned to each device connected to a computer network that
+                  uses the Internet Protocol for communication. An IP address
+                  serves two main functions: host or network interface
+                  identification and location addressing.
+                </p>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </article>
-        <div className="border-t border-border pt-4">
+
+        <footer className="pb-4 pt-2">
           <p className="text-center text-sm text-muted-foreground">
-            Code by{' '}
             <a
-              className="text-foreground underline hover:no-underline"
+              className="transition-colors hover:text-foreground"
               href="https://github.com/franklinrms"
+              rel="noopener noreferrer"
             >
-              Franklin
+              Code by Franklin
             </a>
           </p>
-        </div>
+        </footer>
       </div>
     </main>
   )
